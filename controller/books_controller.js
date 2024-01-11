8f56586585c9fdeea5e8354a401eb210f7f79eaa
unknown <roxanvaz31@gmail.com>
2024-01-10 16:39:33 -0800

    added book model

--- controller/books_controller.js
+++ controller/books_controller.js
@@ -1,9 +1,6 @@
-const express = require('express');
-const cors = require('cors')
 const mongoose = require('mongoose')
-const app = express();
+const { Schema } = mongoose
 
-require('dotenv').config()
-const PORT = process.env.PORT
-app.use(express.json())
-app.use(cors())
+require('Book')
+
+module.exports = booksController
